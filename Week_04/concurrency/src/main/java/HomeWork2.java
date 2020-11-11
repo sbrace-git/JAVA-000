5401dd1935142ff47fc4723df9f9e82a5c69c34d
sbrace <sbrace0822@gmail.com>
2020-11-11 23:16:10 +0800

    week04

---
+++ Week_04/concurrency/src/main/java/HomeWork2.java
@@ -0,0 +1,31 @@
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+public class HomeWork2 {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        long start=System.currentTimeMillis();
+        // 在这里创建一个线程或线程池，
+        // 异步执行 下面方法
+
+        FutureTask futureTask = new FutureTask(() -> sum());
+        futureTask.run();
+
+        // 确保  拿到result 并输出
+        System.out.println("异步计算结果为："+futureTask.get());
+
+        System.out.println("使用时间："+ (System.currentTimeMillis()-start) + " ms");
+
+        // 然后退出main线程
+    }
+
+    private static int sum() {
+        return fibo(36);
+    }
+
+    private static int fibo(int a) {
+        if ( a < 2)
+            return 1;
+        return fibo(a-1) + fibo(a-2);
+    }
+}
