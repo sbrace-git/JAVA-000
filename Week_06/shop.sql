916d7ab786e6b6b29e9d39f945746bb21f63d8b1
sbrace <sbrace0822@gmail.com>
2020-12-02 03:09:44 +0800

    更week 06 update table

--- Week_06/shop.sql
+++ Week_06/shop.sql
@@ -10,19 +10,19 @@ Target Server Type    : MYSQL
 Target Server Version : 80018
 File Encoding         : 65001
 
-Date: 2020-12-01 18:44:56
+Date: 2020-12-02 03:08:32
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for good
+-- Table structure for t_good
 -- ----------------------------
-DROP TABLE IF EXISTS `good`;
-CREATE TABLE `good` (
+DROP TABLE IF EXISTS `t_good`;
+CREATE TABLE `t_good` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
   `name` varchar(255) NOT NULL COMMENT '商品名',
-  `describe` text NOT NULL COMMENT '描述',
+  `detail` varchar(1023) NOT NULL COMMENT '描述',
   `pice` decimal(10,0) unsigned NOT NULL COMMENT '价格',
   `stock` bigint(20) unsigned NOT NULL COMMENT '库存',
   `update_time` bigint(20) unsigned NOT NULL COMMENT '更新时间',
@@ -30,17 +30,17 @@ CREATE TABLE `good` (
   `status` varchar(255) NOT NULL COMMENT '状态',
   `version` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '版本号',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='商品';
+) ENGINE=InnoDB AUTO_INCREMENT=10001 DEFAULT CHARSET=utf8 COMMENT='商品';
 
 -- ----------------------------
--- Table structure for good_snapshot
+-- Table structure for t_good_snapshot
 -- ----------------------------
-DROP TABLE IF EXISTS `good_snapshot`;
-CREATE TABLE `good_snapshot` (
+DROP TABLE IF EXISTS `t_good_snapshot`;
+CREATE TABLE `t_good_snapshot` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
   `good_id` bigint(20) unsigned NOT NULL COMMENT '商品id',
   `name` varchar(255) NOT NULL COMMENT '商品名',
-  `describe` text NOT NULL COMMENT '描述',
+  `describe` varchar(1023) NOT NULL COMMENT '描述',
   `pice` decimal(10,0) unsigned NOT NULL COMMENT '价格',
   `update_time` bigint(20) unsigned NOT NULL COMMENT '更新时间',
   `create_time` bigint(20) unsigned NOT NULL COMMENT '创建时间',
@@ -48,10 +48,10 @@ CREATE TABLE `good_snapshot` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='商品快照';
 
 -- ----------------------------
--- Table structure for order
+-- Table structure for t_order
 -- ----------------------------
-DROP TABLE IF EXISTS `order`;
-CREATE TABLE `order` (
+DROP TABLE IF EXISTS `t_order`;
+CREATE TABLE `t_order` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
   `user_id` bigint(20) unsigned NOT NULL COMMENT '用户id',
   `total` decimal(10,0) unsigned NOT NULL COMMENT '金额',
@@ -59,13 +59,13 @@ CREATE TABLE `order` (
   `update_time` bigint(20) unsigned NOT NULL COMMENT '更新时间',
   `create_time` bigint(20) unsigned NOT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单';
+) ENGINE=InnoDB AUTO_INCREMENT=1200001 DEFAULT CHARSET=utf8 COMMENT='订单';
 
 -- ----------------------------
--- Table structure for order_detail
+-- Table structure for t_order_detail
 -- ----------------------------
-DROP TABLE IF EXISTS `order_detail`;
-CREATE TABLE `order_detail` (
+DROP TABLE IF EXISTS `t_order_detail`;
+CREATE TABLE `t_order_detail` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
   `order_id` bigint(20) unsigned NOT NULL COMMENT '订单id',
   `good_snapshot_id` bigint(20) unsigned NOT NULL COMMENT '商品快照id',
@@ -75,10 +75,10 @@ CREATE TABLE `order_detail` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单详情';
 
 -- ----------------------------
--- Table structure for user
+-- Table structure for t_user
 -- ----------------------------
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
+DROP TABLE IF EXISTS `t_user`;
+CREATE TABLE `t_user` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
   `user_name` varchar(255) NOT NULL COMMENT '用户名',
   `password` varchar(255) NOT NULL COMMENT '密码',
@@ -87,4 +87,4 @@ CREATE TABLE `user` (
   `create_time` bigint(20) unsigned NOT NULL COMMENT '创建时间',
   `status` varchar(255) NOT NULL COMMENT '状态',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户';
+) ENGINE=InnoDB AUTO_INCREMENT=1001 DEFAULT CHARSET=utf8 COMMENT='用户';
