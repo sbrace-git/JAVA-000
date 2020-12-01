ca59b93c0adfe4458b9e91ab71b747ba5f4ab058
sbrace <sbrace0822@gmail.com>
2020-12-01 18:46:11 +0800

    week 06 update shop.sql

--- Week_06/shop.sql
+++ Week_06/shop.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 80018
 File Encoding         : 65001
 
-Date: 2020-11-25 23:46:57
+Date: 2020-12-01 18:44:56
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -25,8 +25,8 @@ CREATE TABLE `good` (
   `describe` text NOT NULL COMMENT '描述',
   `pice` decimal(10,0) unsigned NOT NULL COMMENT '价格',
   `stock` bigint(20) unsigned NOT NULL COMMENT '库存',
-  `update_time` datetime NOT NULL COMMENT '更新时间',
-  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_time` bigint(20) unsigned NOT NULL COMMENT '更新时间',
+  `create_time` bigint(20) unsigned NOT NULL COMMENT '创建时间',
   `status` varchar(255) NOT NULL COMMENT '状态',
   `version` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '版本号',
   PRIMARY KEY (`id`)
@@ -42,8 +42,8 @@ CREATE TABLE `good_snapshot` (
   `name` varchar(255) NOT NULL COMMENT '商品名',
   `describe` text NOT NULL COMMENT '描述',
   `pice` decimal(10,0) unsigned NOT NULL COMMENT '价格',
-  `create_time` datetime NOT NULL COMMENT '创建时间',
-  `update_time` datetime NOT NULL COMMENT '更新时间',
+  `update_time` bigint(20) unsigned NOT NULL COMMENT '更新时间',
+  `create_time` bigint(20) unsigned NOT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='商品快照';
 
@@ -56,8 +56,8 @@ CREATE TABLE `order` (
   `user_id` bigint(20) unsigned NOT NULL COMMENT '用户id',
   `total` decimal(10,0) unsigned NOT NULL COMMENT '金额',
   `status` varchar(255) NOT NULL COMMENT '状态',
-  `update_time` datetime NOT NULL COMMENT '更新时间',
-  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_time` bigint(20) unsigned NOT NULL COMMENT '更新时间',
+  `create_time` bigint(20) unsigned NOT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单';
 
@@ -69,8 +69,8 @@ CREATE TABLE `order_detail` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
   `order_id` bigint(20) unsigned NOT NULL COMMENT '订单id',
   `good_snapshot_id` bigint(20) unsigned NOT NULL COMMENT '商品快照id',
-  `update_time` datetime NOT NULL COMMENT '更新时间',
-  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_time` bigint(20) unsigned NOT NULL COMMENT '更新时间',
+  `create_time` bigint(20) unsigned NOT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单详情';
 
@@ -83,8 +83,8 @@ CREATE TABLE `user` (
   `user_name` varchar(255) NOT NULL COMMENT '用户名',
   `password` varchar(255) NOT NULL COMMENT '密码',
   `nick_name` varchar(255) NOT NULL COMMENT '昵称',
-  `update_time` datetime NOT NULL COMMENT '更新时间',
-  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_time` bigint(20) unsigned NOT NULL COMMENT '更新时间',
+  `create_time` bigint(20) unsigned NOT NULL COMMENT '创建时间',
   `status` varchar(255) NOT NULL COMMENT '状态',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户';
